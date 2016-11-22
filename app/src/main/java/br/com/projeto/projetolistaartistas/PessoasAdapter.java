672ada8fe00665f55cd0b007090f5f9195fdfd0a
Carlos Eduardo Do O Ferreira <carlosdoo15@gmail.com>
2016-11-22 10:27:18 -0300

    Adicionado Adapter no Pessoa Adapter

--- app/src/main/java/br/com/projeto/projetolistaartistas/PessoasAdapter.java
+++ app/src/main/java/br/com/projeto/projetolistaartistas/PessoasAdapter.java
@@ -1,8 +1,6 @@
 package br.com.projeto.projetolistaartistas;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +10,6 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-import java.util.Calendar;
 import java.util.List;
 
 import br.com.projeto.projetolistaartistas.database.AvaliacaoDAO;
@@ -33,28 +30,28 @@ public class PessoasAdapter extends ArrayAdapter<Pessoa> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         AvaliacaoDAO dao = new AvaliacaoDAO(getContext());
-
         Pessoa pessoa = getItem(position);
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.item_pessoa, null);
-        }
-
+        ViewHolder viewHolder;
         double qtdVotos = 0;
         double somaVotos = 0;
         double mediaVotos = 0;
         List<Avaliacao> listaAvaliacoes = dao.listar();
 
-        ImageView imgCapa               = (ImageView) convertView.findViewById(R.id.img_capa);
-        TextView txtNome                = (TextView) convertView.findViewById(R.id.txt_nome);
-        TextView txtEmail               = (TextView) convertView.findViewById(R.id.txt_estado_cidade);
-        TextView txtTelefone            = (TextView) convertView.findViewById(R.id.txt_bio);
-        TextView txtResumo              = (TextView) convertView.findViewById(R.id.txt_resumo);
-        TextView txtMediaNota           = (TextView) convertView.findViewById(R.id.txt_media_votos);
-        TextView txt_nota               = (TextView) convertView.findViewById(R.id.txt_nota);
-        TextView txt_estado_cidade      = (TextView) convertView.findViewById(R.id.txt_estado_cidade);
-
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.item_pessoa, null);
+            viewHolder = new ViewHolder();
+            viewHolder.imgCapa = (ImageView) convertView.findViewById(R.id.img_capa);
+            viewHolder.txtNome = (TextView) convertView.findViewById(R.id.txt_nome);
+            viewHolder.txtEmail = (TextView) convertView.findViewById(R.id.txt_estado_cidade);
+            viewHolder.txtTelefone = (TextView) convertView.findViewById(R.id.txt_bio);
+            viewHolder.txtResumo = (TextView) convertView.findViewById(R.id.txt_resumo);
+            viewHolder.txtMediaNota = (TextView) convertView.findViewById(R.id.txt_media_votos);
+            viewHolder.txt_nota = (TextView) convertView.findViewById(R.id.txt_nota);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
         /*if (pessoa.getAvaliacao() != null) {
             qtdVotos = pessoa.getAvaliacao().size();
 
@@ -70,35 +67,42 @@ public class PessoasAdapter extends ArrayAdapter<Pessoa> {
                 }
             }
         }*/
-
         //Aplica a nota do artista
         for (int i = 0; i < listaAvaliacoes.size(); i++) {
             if (listaAvaliacoes.get(i).getUsu_id_artista() == pessoa.getUsu_id()) {
                 if(listaAvaliacoes.size() != 0) {
-                    txt_nota.setText(String.format("Sua nota: " + "%.1f", listaAvaliacoes.get(i).getAva_nota()));
+                    viewHolder.txt_nota.setText(String.format("Sua nota: " + "%.1f", listaAvaliacoes.get(i).getAva_nota()));
                     somaVotos = somaVotos + listaAvaliacoes.get(i).getAva_nota();
                     qtdVotos = qtdVotos + 1;
                 }
             }
         }
-
-
-        txtNome.setText(pessoa.getUsu_nome());
+        viewHolder.txtNome.setText(pessoa.getUsu_nome());
         if (pessoa.getAvaliacao().size() != 0) {
-            txtNome.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_avaliado, 0);
+            viewHolder.txtNome.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_avaliado, 0);
         }
 
-        txtEmail.setText(pessoa.getUsu_email());
-        txtTelefone.setText(pessoa.getUsu_telefone());
-        txtResumo.setText("");
+        viewHolder.txtEmail.setText(pessoa.getUsu_email());
+        viewHolder.txtTelefone.setText(pessoa.getUsu_telefone());
+        viewHolder.txtResumo.setText("");
 
         if (mediaVotos != 0) {
-            txtMediaNota.setText(String.format("%.1f", mediaVotos) + "/10");
+            viewHolder.txtMediaNota.setText(String.format("%.1f", mediaVotos) + "/10");
         }
 
         //img do artista
-        Glide.with(getContext()).load(pessoa.getUsu_imagem()).into(imgCapa);
+        Glide.with(getContext()).load(pessoa.getUsu_imagem()).into(viewHolder.imgCapa);
 
         return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView imgCapa;
+        TextView txtNome;
+        TextView txtEmail;
+        TextView txtTelefone;
+        TextView txtResumo;
+        TextView txtMediaNota;
+        TextView txt_nota;
     }
 }
